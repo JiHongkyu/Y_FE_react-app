a064ce0c3ddcdc62d62cb8294b52234628ec7033
Hongkyu Ji <121606131+JiHongkyu@users.noreply.github.com>
2023-08-21 23:48:59 +0900

    Add files via upload

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './ExpenseItem.css';
+import { MdDelete, MdEdit } from 'react-icons/md';
+
+export default class ExpenseItem extends Component {
+  render() {
+    return (
+      <li className='item'>
+        <div className='info'>
+          <span className='expense'></span>
+          <span className='amount'>원</span>
+        </div>
+        <div>
+          <button className='edit-btn'>
+            <MdEdit />
+          </button>
+          <button className='clear-btn'>
+            <MdDelete />
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
